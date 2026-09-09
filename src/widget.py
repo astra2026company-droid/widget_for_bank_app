2d468414def2ae65d62c8413a1cffc602ff460c7
Maksim <n6541270@gmail.com>
2026-09-09 15:25:51 +0900

    add get_date in widget.py

--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,4 @@
-from src.masks import get_mask_card_number, get_mask_account
+from masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(type_and_number: str) -> str:
@@ -9,8 +9,16 @@ def mask_account_card(type_and_number: str) -> str:
     total_mask = payment_source + " "
 
     if payment_source.lower() == "счет" or payment_source.lower() == "счёт":
-         total_mask += get_mask_account(numbers_source)
+        total_mask += get_mask_account(numbers_source)
     else:
         total_mask += get_mask_card_number(numbers_source)
 
     return total_mask
+
+
+def get_date(date: str) -> str:
+    """Функция, которая форматирует дату"""
+    list_date = date.split("T")[0].split("-")
+    list_date = list_date[::-1]
+
+    return ".".join(list_date)
