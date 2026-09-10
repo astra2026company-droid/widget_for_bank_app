a39284b4af39f1e5bbb5319bda9e974213024b6e
Maksim <n6541270@gmail.com>
2026-09-10 16:03:15 +0900

    add new func sort_by_date in module proccessing.py

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,7 @@
+from widget import get_date
+from datetime import datetime
+
+
 def filter_by_state(list_dicts: list, key_value: str = 'EXECUTED') -> list:
     """Функция, которая сортирует список словарей по ключу"""
     sorted_list_dicts = []
@@ -7,3 +11,9 @@ def filter_by_state(list_dicts: list, key_value: str = 'EXECUTED') -> list:
             sorted_list_dicts.append(collection)
 
     return sorted_list_dicts
+
+
+def sort_by_date(list_dicts: list, is_reverse: bool = True) -> list:
+    """Функция, которая сортирует список по дате"""
+    list_date_sorted = sorted(list_dicts, key=lambda dict_items: dict_items["date"], reverse=is_reverse)
+    return list_date_sorted
