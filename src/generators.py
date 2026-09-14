3b19be58d50ee56afb0be5634e4ad7853a57760f
Maksim Spirin <astra2026company@gmail.com>
2026-09-14 22:30:00 +0900

    add card_number_generator in generators.py

--- src/generators.py
+++ src/generators.py
@@ -12,3 +12,28 @@ def transaction_descriptions(list_trans: list) -> str:
     """Функция, которая возвращает описание каждой операции по очереди"""
     for coll in list_trans:
         yield coll["description"]
+
+
+def card_number_generator(start: int = 1, stop: int = 9) -> str:
+    """Функция, которая выдает номера карт в заданом диапазоне в формате 0000 0000 0000 0000"""
+    generator_card_number = list(("0" * (16 - len(str(num))) + str(num) for num in range(start, stop + 1)))
+    generator_card_number_new = []
+
+    for card_number in generator_card_number:
+        new_card_number = []
+        count_step = 0
+
+        for num in card_number:
+            if count_step == 4:
+                count_step = 0
+
+                new_card_number.append(" ")
+
+            new_card_number.append(num)
+
+            count_step += 1
+
+        new_card_number = "".join(new_card_number)
+        generator_card_number_new.append(new_card_number)
+
+    return generator_card_number_new
